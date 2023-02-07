0da2c1161bb2556348bc0339fa5268fd62900889
takaharu yamamoto <yamataka22@gmail.com>
2023-02-07 18:13:33 +0900

    first commit

---
+++ db/migrate/20230207080823_create_customers.rb
@@ -0,0 +1,12 @@
+class CreateCustomers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :customers do |t|
+      t.string :name
+      t.string :email
+      t.string :address
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
